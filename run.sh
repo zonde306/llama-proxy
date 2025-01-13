2552c066149704c2f1f66386558d9a364c9a06b1
zonde306 <anonymous@localhost>
2025-01-13 11:02:03 +0800

    fix: launcher scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-source /opt/rh/rh-python38/enable
-python3.8 -m uvicorn llama:app --host localhost --port 25010 --reload
+python3 -m venv create
+source ./.venv/bin/activate
+python3 -m pip install -r requirements.txt
+python3 -m uvicorn llama:app --host localhost --port 25010 --reload
