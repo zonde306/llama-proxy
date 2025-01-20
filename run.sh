dde0673b88a4864454acdfa5af7bae1cfc49bd11
zonde306 <anonymous@localhost>
2025-01-20 16:31:03 +0800

    fix: command mismatch

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m venv create
-source ./.venv/bin/activate
+python3 -m venv venv
+source ./venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 -m uvicorn llama:app --host localhost --port 25010 --reload
